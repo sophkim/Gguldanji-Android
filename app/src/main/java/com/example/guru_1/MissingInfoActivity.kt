be442c29e0e12c25b941fb5c43b21c32ec260083
kimbona <sophkim>
2022-01-27 20:32:20 +0900

    xml design

---
+++ app/src/main/java/com/example/guru_1/MissingInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.guru_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MissingInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_missing_info)
+    }
+}
