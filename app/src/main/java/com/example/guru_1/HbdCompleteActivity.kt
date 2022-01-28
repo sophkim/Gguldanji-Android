3bd30d72792e83330f66b41a98c1b5e4be6825b8
kimbona <sophkim>
2022-01-28 13:26:59 +0900

    xml design

---
+++ app/src/main/java/com/example/guru_1/HbdCompleteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.guru_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HbdCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hbd_complete)
+    }
+}
