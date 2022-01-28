6d25904c8096c1a3ea2feb3d002ecae31b9b5713
zada8 <qqaz0609@naver.com>
2022-01-29 02:12:54 +0900

    hbdImage

--- app/src/main/java/com/example/guru_1/HbdCompleteActivity.kt
+++ app/src/main/java/com/example/guru_1/HbdCompleteActivity.kt
@@ -1,11 +1,27 @@
 package com.example.guru_1
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class HbdCompleteActivity : AppCompatActivity() {
+
+    lateinit var hbdImage: ImageView
+    lateinit var hbdImage2: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hbd_complete)
+
+        hbdImage = findViewById(R.id.hbdCom_img)
+        hbdImage2 = findViewById(R.id.hbdCom_img2)
+
+
+        val byteArray = intent.getByteArrayExtra("image")
+        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+
+        hbdImage.setImageBitmap(bitmap)
+        hbdImage2.setImageBitmap(bitmap)
     }
 }
