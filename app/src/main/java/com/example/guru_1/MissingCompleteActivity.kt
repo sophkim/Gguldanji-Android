b827e53da7e106b75b5c51c79b294453531dc4a0
zada8 <qqaz0609@naver.com>
2022-01-29 02:48:43 +0900

    missing image

--- app/src/main/java/com/example/guru_1/MissingCompleteActivity.kt
+++ app/src/main/java/com/example/guru_1/MissingCompleteActivity.kt
@@ -1,11 +1,23 @@
 package com.example.guru_1
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MissingCompleteActivity : AppCompatActivity() {
+
+    lateinit var missingImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_missing_complete)
+
+        missingImage = findViewById(R.id.missingCom_img)
+
+        val byteArray = intent.getByteArrayExtra("image")
+        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+
+        missingImage.setImageBitmap(bitmap)
     }
 }
